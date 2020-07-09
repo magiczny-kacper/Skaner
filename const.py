de105e237b1a7f23aaff0b25041988505ab7c0ce
magiczny-kacper <kapibrv97@gmail.com>
2020-07-09 23:28:47 +0200

    Dodane wartości stałe

--- const.py
+++ const.py
@@ -5,5 +5,71 @@ definitValue = 0.0
 defStep = 0.0
 
 # Stałe GUI
+
+# Etykiety parametrów
 sampNumLabRow = 0
 sampNumLabCol = 0
+
+sampIntLabRow = 1
+sampIntLabCol = 0
+
+iniValLabRow = 2
+iniValLabCol = 0
+
+delValLabRow = 3
+delValLabCol = 0
+
+# Pola do wpisania parametrów
+sampNumEditRow = 0
+sampNumEditCol = 1
+
+sampIntEditRow = 1
+sampIntEditCol = 1
+
+iniValEditRow = 2
+iniValEditCol = 1
+
+delValEditRow = 3
+delValEditCol = 1
+
+# Aktualne wartości X i Y
+currXLabRow = 3
+currXLabCol = 2
+currXLabRowDim = 1
+currXLabColDim = 3
+
+currYLabRow = 4
+currYLabCol = 2
+currYLabRowDim = 1
+currYLabColDim = 3
+
+# Przyciski
+startBtnRow = 0
+startBtnCol = 2
+
+stopBtnRow = 1
+stopBtnCol = 2
+
+clearBtnRow = 2
+clearBrnCol = 2
+
+# Wybór skali wykresu
+pltXsclRow = 4
+pltXsclCol = 0
+pltXsclRowDim = 1
+pltXsclColDim = 1
+
+pltYsclRow = 4
+pltYsclCol = 1
+pltYsclRowDim = 1
+pltYsclColDim = 1
+
+# Umieszczenie wykresu
+canvWid = 5
+canvHght = 4
+canvDpi = 6
+
+canvRow = 5
+canvCol = 0
+canvRowDim = 6
+canvColDim = 6
